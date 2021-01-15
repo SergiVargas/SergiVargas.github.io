e8b3b5284cfc6709a690b08d94468d76cc733d7b
SergioV82 <sergiovargasmateos@gmail.com>
2021-01-15 18:18:16 +0100

    second webApp

--- DataScience/StreamlitProjects/README.md
+++ DataScience/StreamlitProjects/README.md
@@ -1,6 +1,6 @@
 ### INFORMATION
 
-Streamlit is a great Python library for quickly creating beautiful data science web applications. When installing it and starting the project, the WebApp will open in our browser. For more information visit https://www.streamlit.io/. We must follow the following steps:
+Streamlit is a great Python library for quickly creating beautiful data science web applications. When installing it and starting the project, the Web App will open in our browser. For more information visit https://www.streamlit.io/. We must follow the following steps:
 
 1. Install Streamlit `$pip install Streamlit`
 2. We can test if it is properly installed using `$streamlit Hello`. This will open a window of our browser
--- DataScience/StreamlitProjects/firstApp.py
+++ DataScience/StreamlitProjects/firstApp.py
@@ -1,17 +1,17 @@
-#This is a simple App to show how we can quickly create a nice WebApp
+#This is a simple App to show how we can quickly create a nice Web App
 #This App shows the closing price and the volume of Google in a period of time
 
 import pandas as pd
 import streamlit as st
-import yfinance as yf #We need install this package for this WebbApp
+import yfinance as yf #We need install this package for this Webb App
 
 
 
-#This the header of the WebApp, here we can write what we want to show in the App
+#This the header of the Web App, here we can write in Markdown what we want to show in the App
 st.write("""
-         #Simple Stock Price App
+         # Simple Stock Price App
          
-         Shows the closing price and volume of google stock for ten years
+         Shows the **closing price** and **volume** of google stock for ten years
          
          """)
 
@@ -22,7 +22,17 @@ tickerData = yf.Ticker(tickerSymbol)
 
 tickerDF = tickerData.history(period= '1d', start = '2010-5-31', end = '2020-5-31')
 
-#Create the closing price chart in the WebApp
+
+st.write("""
+         ## Closing Price
+         """)
+
+#Create the closing price chart in the Web App
 st.line_chart(tickerDF.Close)
-#Create the volumen chart in the WebApp
+
+st.write("""
+         ## Volumen
+         """)
+
+#Create the volumen chart in the Web App
 st.line_chart(tickerDF.Volume)
