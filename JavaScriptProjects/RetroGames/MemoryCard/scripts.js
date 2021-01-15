73410b97826536c7c9d4c8b65058fd3894b46576
SergioV82 <sergiovargasmateos@gmail.com>
2021-01-16 00:27:28 +0100

    memory v3

--- JavaScriptProjects/RetroGames/MemoryCard/scripts.js
+++ JavaScriptProjects/RetroGames/MemoryCard/scripts.js
@@ -1,8 +1,25 @@
 
 const cards = document.querySelectorAll('.memory-card');
 
+
+let hasFlipperCard = false;
+let firstCard, secondCard;
+
+
 function flipCards() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  //first click
+  if(!hasFlipperCard){
+
+    hasFlipperCard = true;
+    firstCard = this;
+
+  }else{
+      //second click
+      hasFlipperCard = false;
+      secondCard = this;
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCards));
