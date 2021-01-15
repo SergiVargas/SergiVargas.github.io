6a270ccac9be767848a3c109dbd4b2dd3ecd95bd
SergioV82 <sergiovargasmateos@gmail.com>
2021-01-16 00:13:50 +0100

    memory v2

---
+++ JavaScriptProjects/RetroGames/MemoryCard/scripts.js
@@ -0,0 +1,8 @@
+
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCards() {
+  this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCards));
