232060d484af96857c5c8200d66de3a7842aa7dd
SergioV82 <sergiovargasmateos@gmail.com>
2021-01-15 13:24:43 +0100

    second App

---
+++ DataScience/StreamlitProject/README.md
@@ -0,0 +1,7 @@
+### INFORMATION
+
+Streamlit is a great Python library for quickly creating beautiful data science web applications. When installing it and starting the project, the WebApp will open in our browser. For more information visit https://www.streamlit.io/. We must follow the following steps:
+
+1. Install Streamlit `$pip install Streamlit`
+2. We can test if it is properly installed using `$streamlit Hello`. This will open a window of our browser
+3. We start the projects with `$streamlit run aplication-name`
--- DataScience/StreamlitProject/firstApp.py
+++ DataScience/StreamlitProject/firstApp.py
@@ -1,28 +1,28 @@
-#This is a simple App to show how We can create quickly a beatiful WebApp
-#This App show the close price and volume of Google in a period of time
+#This is a simple App to show how we can quickly create a nice WebApp
+#This App shows the closing price and the volume of Google in a period of time
 
 import pandas as pd
 import streamlit as st
-import yfinance as yf #We need install this package for this App
+import yfinance as yf #We need install this package for this WebbApp
 
 
 
-#This the header of WebApp, here we can write that we want to show in el App
+#This the header of the WebApp, here we can write what we want to show in the App
 st.write("""
          #Simple Stock Price App
          
-         Shown are the stock closing price and volumen of Google Action in during ten years
-         
+         Shows the closing price and volume of google stock for ten years
          
          """)
 
 
-tickerSymbol = 'GOOGL' #The symbol of Google in Stock Market
+tickerSymbol = 'GOOGL' #The Google symbol in Stock Market
 
 tickerData = yf.Ticker(tickerSymbol)
 
 tickerDF = tickerData.history(period= '1d', start = '2010-5-31', end = '2020-5-31')
-#Create the chart of closing price in WebApp
+
+#Create the closing price chart in the WebApp
 st.line_chart(tickerDF.Close)
-#Create the chart of volumen in WebApp
+#Create the volumen chart in the WebApp
 st.line_chart(tickerDF.Volume)
