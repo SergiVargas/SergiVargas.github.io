b91cd3c911c70f320e923b75fc92bff8ba509a0a
SergioV82 <sergiovargasmateos@gmail.com>
2021-01-24 15:39:46 +0100

    aplica

--- JavaProgramming/Sudoku/src/sudoku/userinterface/SudokuTextField.java
+++ JavaProgramming/Sudoku/src/sudoku/userinterface/SudokuTextField.java
@@ -1,7 +1,26 @@
 
 package sudoku.userinterface;
 
+import javafx.scene.control.TextField;
 
-public class SudokuTextField {
+
+public class SudokuTextField extends TextField {
     
+    private final int x;
+    private final int y;
+    
+    public SudokuTextField(int x, int y){
+        
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
 }
+ 
